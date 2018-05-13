d399fb4e23ee6a6f831bf65ef27204b5021aac83
Evgeniy <asteroidsuper@gmail.com>
2018-05-13 18:29:59 +0700

    Fixed alignments of default products init list

--- generic_alg_lab_6/product.cpp
+++ generic_alg_lab_6/product.cpp
@@ -30,18 +30,18 @@ double Product::weight() const
 
 const QList<Product>& Product::defaultProducts()
 {
-	static QList<Product> products
-	{//			lstart	k		cost	weight	name
-		Product(10,		0.6,	49,		1,		"Milk"),
-		Product(5,		0.7,	21,		0.6,	"Bread"),
-		Product(9,		0.5,	60,		0.3,	"Eggs"),
-		Product(3,		0.9,	50,		0.1,	"Tea"),
-		Product(5,		0.4,	65,		1,		"Sugar"),
-		Product(7,		0.45,	40,		0.3,	"Sour Cream"),
-		Product(5,		0.8,	35,		0.25,	"Ice Cream"),
-		Product(3,		0.5,	55,		1,		"Cola"),
-		Product(2,		0.9,	80,		0.5,	"Beer"),
-		Product(5,		0.75,	95,		1,		"Butter")
+    static QList<Product> products
+    {//         lstart  k       cost    weight  name
+        Product(10,     0.6,    49,     1,      "Milk"),
+        Product(5,      0.7,    1,      0.6,    "Bread"),
+        Product(9,      0.5,    60,     0.3,    "Eggs"),
+        Product(3,      0.9,    50,     0.1,    "Tea"),
+        Product(5,      0.4,    65,     1,      "Sugar"),
+        Product(7,      0.45,   40,     0.3,    "Sour Cream"),
+        Product(5,      0.8,    35,     0.25,   "Ice Cream"),
+        Product(3,      0.5,    55,     1,      "Cola"),
+        Product(2,      0.9,    80,     0.5,	"Beer"),
+        Product(5,      0.75,   95,     1,      "Butter")
 	};
 
 	return products;
